362ec63dd9e83a5458d79403e6d147c6a0b29941
Saadia <sacdiamohamed083@gmail.com>
2026-02-18 10:17:22 -0800

    done

---
+++ public/App.js
@@ -0,0 +1,21 @@
+function Navbar() {
+  return <nav>Navbar</nav>;
+}
+
+function Home() {
+  return (
+    <div id="home">
+      <h1>Home</h1>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      {/* Components will go here */}
+    </div>
+  );
+}
+
+export default App;
